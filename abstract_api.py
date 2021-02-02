ac0902b51d21578007eb8d8cbbe5d189e8c10356
Saffat Hasan <saffathasan@gmail.com>
2021-02-02 14:51:53 -0500

    chore: remove dead code

--- abstract_api.py
+++ abstract_api.py
@@ -65,7 +65,4 @@ class AbstractAPI(ABC):
         return self.competition_base_url.format(competition_id=competition_id)
 
     def update_player_xp(self, name):
-        #if name.lower() == 'pvm fidoz':
-        #    return
-        #print(f"Updating {name}...")
         requests.get(self.update_player_base_url.format(name=name))
